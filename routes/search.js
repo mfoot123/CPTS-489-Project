d2073f89365ce8d03a289b68b2b6ef201f5f0d5a
Tanner Blanken <tanner.blanken@wsu.edu>
2024-04-21 15:33:44 -0700

    Implemented searching by a keyword.

--- routes/search.js
+++ routes/search.js
@@ -10,4 +10,12 @@ router.get('/', async function(req, res, next) {
     res.render('search', {categories} );
 });
 
+router.get('/results', async function(req, res, next) {
+    const keyword = req.query.search.toLowerCase();
+    const products = await Product.findAll({
+        where: sequelize.where( sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', '%' + keyword + '%')
+    });
+    res.render('products', {products} );
+});
+
 module.exports = router;
