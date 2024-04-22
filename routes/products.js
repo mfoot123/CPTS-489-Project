54d28bc16783e73755b7252fcfe2daff4d892a75
Tanner Blanken <tanner.blanken@wsu.edu>
2024-04-21 19:24:52 -0700

    Added error checking to each function in the routes.

--- routes/products.js
+++ routes/products.js
@@ -2,20 +2,38 @@ var express = require('express');
 var router = express.Router();
 const Product = require('../models/Product');
 
+const sessionChecker = (req, res, next)=>{
+    if(req.session.user){
+      res.locals.username = req.session.user.username
+      next()
+    }else{
+      res.redirect("/?msg=raf")
+    }
+  }
+  
+router.use(sessionChecker)
+
 router.get('/', async function(req, res, next) {
-    const categoryName = req.query.category;
-    const products = await Product.findAll({
-        where: {
-            category: categoryName,
-        },
-    });
-    res.render('products', {products} );
+    try{
+        const categoryName = req.query.category;
+        const products = await Product.findAll({
+            where: {
+                category: categoryName,
+            },
+        });
+        res.render('products', {products} );
+    }
+    catch(error){
+        console.log(error.message);
+        res.redirect("/?msg=incorrect+category+name")
+    }
 });
 router.post('/new', async function(req, res, next)
 {
-        console.log(req.body.name);
+    try{
+        //console.log(req.body.name);
         const {category, name, description, price, imageUrl, guideUrl} = req.body;
-        const product = await Product.create(
+        await Product.create(
             {
                 category,
                 name,
@@ -25,7 +43,13 @@ router.post('/new', async function(req, res, next)
                 guideUrl
             }
         );
-        res.redirect("/search")
+        res.redirect("/search");        
+    }
+    catch(error)
+    {
+        console.log(error.message);
+        res.redirect("/?msg=failed+to+validate+product");
+    }
 });
 
 module.exports = router;
--- routes/search.js
+++ routes/search.js
@@ -3,19 +3,44 @@ var router = express.Router();
 const Product = require('../models/Product');
 const sequelize = require('../db');
 
+const sessionChecker = (req, res, next)=>{
+    if(req.session.user){
+      res.locals.username = req.session.user.username
+      next()
+    }else{
+      res.redirect("/?msg=raf")
+    }
+  }
+  
+router.use(sessionChecker)
+
 router.get('/', async function(req, res, next) {
-    const categories = await Product.findAll({
-        attributes: [[sequelize.fn('DISTINCT', sequelize.col('category')), 'category']]
-    });
-    res.render('search', {categories} );
+    try{
+        const categories = await Product.findAll({
+            attributes: [[sequelize.fn('DISTINCT', sequelize.col('category')), 'category']]
+        });
+        res.render('search', {categories} );
+    }
+    catch(error)
+    {
+        console.log(error.message);
+        res.redirect("/?msg=failed+to+find+products");
+    }
 });
 
 router.get('/results', async function(req, res, next) {
-    const keyword = req.query.search.toLowerCase();
-    const products = await Product.findAll({
-        where: sequelize.where( sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', '%' + keyword + '%')
-    });
-    res.render('products', {products} );
+    try{
+        const keyword = req.query.search.toLowerCase();
+        const products = await Product.findAll({
+            where: sequelize.where( sequelize.fn('LOWER', sequelize.col('name')), 'LIKE', '%' + keyword + '%')
+        });
+        res.render('products', {products} );
+    }
+    catch(error)
+    {
+        console.log(error);
+        res.redirect("/?msg=no+products+contain+that+keyword")
+    }
 });
 
 module.exports = router;
