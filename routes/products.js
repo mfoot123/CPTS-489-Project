1f1dcf0508f6004c52d9f0280667353c2b529b32
Josh Abbott <122472243+Josh-Abbott@users.noreply.github.com>
2024-04-21 18:06:28 -0700

    Added session checking to products

--- routes/products.js
+++ routes/products.js
@@ -2,6 +2,17 @@ var express = require('express');
 var router = express.Router();
 const Product = require('../models/Product');
 
+const sessionChecker = (req, res, next)=>{
+    if(req.session.user){
+      res.locals.username = req.session.user.username
+      next()
+    }else{
+      res.redirect("/?msg=raf")
+    }
+  }
+  
+  router.use(sessionChecker)
+
 router.get('/', async function(req, res, next) {
     console.log("IS IT MAKING IT HERE");
     const categoryName = req.query.category;
